858777afa4b7bc0b3d0c7b9530a74daa16d986f4
Llebac <jc6036@gmail.com>
2015-05-25 17:07:18 -0400

    Added files for input valdiation. Functions will be developed in the future.

---
+++ error.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <exception>
+
+using namespace std;
+
+int single_character_validation (char input) {
+// This makes sure the user has entered a lowercase single character that is a letter, not a number.
+// This function returns 0 if the letter passes validation, and returns 1 if the letter does not.
+// The function falls back on returning 2 if it can't return 1 or 0.
+
+
+
+    return 2;
+}
---
+++ error.h
@@ -0,0 +1,9 @@
+#ifndef ERROR_H_
+#define ERROR_H_
+
+int single_character_validation (char input);
+
+
+
+
+#endif // End of error.h definition
